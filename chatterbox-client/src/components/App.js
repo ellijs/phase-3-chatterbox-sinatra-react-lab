c1dc1ae9efd40bbd603039967fb6d6eaad8595f6
ellijs <ellijs@hotmail.com>
2021-08-21 14:38:15 -0400

    completed Assignment

--- chatterbox-client/src/components/App.js
+++ chatterbox-client/src/components/App.js
@@ -12,7 +12,7 @@ function App() {
   const [search, setSearch] = useState("");
 
   useEffect(() => {
-    fetch("http://localhost:4000/messages")
+    fetch("http://localhost:9292/messages")
       .then((r) => r.json())
       .then((messages) => setMessages(messages));
   }, []);
@@ -37,6 +37,7 @@ function App() {
     setMessages(updatedMessages);
   }
 
+  
   const displayedMessages = messages.filter((message) =>
     message.body.toLowerCase().includes(search.toLowerCase())
   );
